c0ac5d38c7c3d1c94f4cb1f087bee10981f1c13d
Aswin Ramabadran <ramabadranaswin2004@gmail.com>
2025-08-21 20:50:30 +0530

    Add lookup tables and Types(Square and Piece types)

--- engine/include/game.hpp
+++ engine/include/game.hpp
@@ -4,34 +4,39 @@
 #include "board.hpp"
 
 class Game {
-  private:
-    Board& board;
-    bool playerTurn;
-    bool whiteInCheck;
-    bool blackInCheck;
-    bool checkMate;
-    bool draw;
+private:
+  Board &board;
+  bool playerTurn;
+  bool whiteInCheck;
+  bool blackInCheck;
+  bool checkMate;
+  bool draw;
 
-  public:
-    Game(Board& board) : board(board), playerTurn(true), whiteInCheck(false), blackInCheck(false), checkMate(false), draw(false) {};
-    inline void switchTurn() { playerTurn = !playerTurn; }
-    ~Game() = default;
+public:
+  Game(Board &board)
+      : board(board), playerTurn(true), whiteInCheck(false),
+        blackInCheck(false), checkMate(false), draw(false) {};
+  inline void switchTurn() { playerTurn = !playerTurn; }
+  ~Game() = default;
 
-    // getters and setters for Game state
-    inline bool isPlayerTurn() const { return playerTurn; }
-    inline void setPlayerTurn(bool playerTurn) { this->playerTurn = playerTurn; }
+  // getters and setters for Game state
+  inline bool isPlayerTurn() const { return playerTurn; }
+  inline void setPlayerTurn(bool playerTurn) { this->playerTurn = playerTurn; }
 
-    inline bool isWhiteInCheck() const { return whiteInCheck; }
-    inline void setWhiteInCheck(bool whiteInCheck) { this->whiteInCheck = whiteInCheck; }
+  inline bool isWhiteInCheck() const { return whiteInCheck; }
+  inline void setWhiteInCheck(bool whiteInCheck) {
+    this->whiteInCheck = whiteInCheck;
+  }
 
-    inline bool isBlackInCheck() const { return blackInCheck; }
-    inline void setBlackInCheck(bool blackInCheck) { this->blackInCheck = blackInCheck; }
+  inline bool isBlackInCheck() const { return blackInCheck; }
+  inline void setBlackInCheck(bool blackInCheck) {
+    this->blackInCheck = blackInCheck;
+  }
 
-    inline bool isCheckMate() const { return checkMate; }
-    inline void setCheckMate(bool checkMate) { this->checkMate = checkMate; }
+  inline bool isCheckMate() const { return checkMate; }
+  inline void setCheckMate(bool checkMate) { this->checkMate = checkMate; }
 
-    inline bool isDraw() const { return draw; }
-    inline void setDraw(bool draw) { this->draw = draw; }
-    
+  inline bool isDraw() const { return draw; }
+  inline void setDraw(bool draw) { this->draw = draw; }
 };
 #endif
---
+++ engine/include/move.hpp
@@ -0,0 +1,7 @@
+#ifndef MOVE_HPP
+#define MOVE_HPP
+
+#include "../include/board.hpp"
+
+class Move {};
+#endif
